93d2d0510c8629bbf9212c25af95cf1a60aa10b1
Xueg <xueg@ymify.com>
2023-08-21 10:50:49 +0800

    first commit

---
+++ src/const/website.js
@@ -0,0 +1,15 @@
+export default {
+  title: '招商服务中心系统',
+  logo: 'N',
+  author: '',
+  whiteList: ['/login', '/404', '/401', '/lock'],
+  lockPage: '/lock',
+  info: {
+    title: '招商服务中心系统',
+    list: []
+  },
+  wel: {
+    title: '招商服务中心系统',
+    list: []
+  }
+}
---
+++ src/router/index.js
@@ -0,0 +1,133 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// const _import = require('./_import_' + process.env.NODE_ENV)
+// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
+// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
+
+Vue.use(Router)
+
+/* layout */
+import layout from '../views/layout/Layout'
+
+/**
+* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+*                                if not set alwaysShow, only more than one route under the children
+*                                it will becomes nested mode, otherwise not show the root menu
+* redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
+* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* meta : {
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar,
+  }
+**/
+export const constantRouterMap = [
+  {
+    path: '/',
+    redirect: '/welcome/welcome'
+  },
+  {
+    path: '/welcome',
+    component: layout,
+    name: '首页',
+    meta: { title: '首页', icon: 'dashboard' },
+    children: [
+      {
+        path: 'welcome',
+        name: 'welcome',
+        component: () => import('@/views/common/welcome'),
+        meta: { title: '首页', icon: 'dashboard' }
+      }
+    ]
+  },
+  { path: '/login', component: () => import('@/views/login'), name: '登录NxAdmin', hidden: true },
+  // 锁屏
+  {
+    path: '/lock',
+    hidden: true,
+    name: '锁屏页',
+    component: () => import('@/views/common/lock')
+  },
+  { path: '/401', component: () => import('@/views/error-page/401'), name: 'page401', hidden: true, meta: { title: 'page401', noCache: true }},
+  { path: '/404', component: () => import('@/views/error-page/404'), name: 'page404', hidden: true, meta: { title: 'page404', noCache: true }}
+]
+
+export default new Router({
+  mode: 'history', // 后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
+export const asyncRouterMap = [
+  {
+    path: '/property',
+    component: layout,
+    redirect: 'noredirect',
+    name: '物业管理',
+    key: 'Property',
+    meta: {
+      title: '物业管理',
+      icon: 'chart'
+    },
+    children: [{
+      path: 'propertyList',
+      component: () => import('@/views/property/propertyList'),
+      name: '物业列表',
+      key: '@Get:lv_property_propertyList',
+      meta: {
+        title: '物业列表',
+        icon: 'documentation'
+      }
+    }]
+  }, {
+    path: '/system',
+    component: layout,
+    redirect: 'noredirect',
+    name: '系统管理',
+    key: 'System', // 权限判断使用
+    meta: {
+      title: '系统管理',
+      icon: 'system'
+    },
+    children: [{
+      path: 'permission',
+      component: () => import('@/views/system/permission'),
+      name: '权限列表',
+      key: '@Get:lv_permissions',
+      // hidden: true,
+      meta: {
+        title: '权限列表',
+        icon: 'lock'
+      }
+    }, {
+      path: 'role',
+      component: () => import('@/views/system/role'),
+      name: '角色列表',
+      key: '@Get:lv_roles',
+      // hidden: true,
+      meta: {
+        title: '角色列表',
+        icon: 'table'
+      }
+    }, {
+      path: 'user',
+      component: () => import('@/views/system/user'),
+      name: '用户列表',
+      key: '@Get:lv_users',
+      meta: {
+        title: '用户列表',
+        icon: 'peoples'
+      }
+    }, {
+      path: 'log',
+      component: () => import('@/views/system/log'),
+      name: '操作日志',
+      key: '@Get:lv_logs',
+      // hidden: true,
+      meta: {
+        title: '操作日志',
+        icon: 'form'
+      }
+    }]
+  }
+]
